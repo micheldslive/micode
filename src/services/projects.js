7c59686a6151a4ca266898b95935f4c2266a2dcb
Michel Domingos <micheldslive@gmail.com>
2021-12-22 09:32:26 -0300

    picinguaba village added

--- src/services/projects.js
+++ src/services/projects.js
@@ -1,3 +1,4 @@
+import Picinguaba from "assets/images/picinguaba.jpg";
 import Nubank from "assets/images/nubank.jpg";
 import Netflix from "assets/images/netflix.jpg";
 import Gamalearning from "assets/images/gamalearning.jpg";
@@ -5,46 +6,53 @@ import CascaGrossa from "assets/images/cascagrossa.jpg";
 import Workpoint from "assets/images/workpoint.jpg";
 import Venom from "assets/images/venom.jpg";
 
-export { Nubank, Gamalearning, CascaGrossa, Workpoint, Venom, Netflix };
+export { Picinguaba, Nubank, Gamalearning, CascaGrossa, Workpoint, Venom, Netflix };
 
 export const projects = [
   {
     id: 1,
+    name: "Vila Picinguaba",
+    icon: Picinguaba,
+    text: "Projeto final do Gama Experience #39. A proposta foi desenvolver um guia turístico navegável sobre a Vila Picinguaba, com dicas sobre acomodações, roteiros, eventos e pontos para alimentação, mostrando um pouco de tudo que a Vila tem a oferecer.",
+    href: "https://vilapicinguaba.vercel.app/",
+  },
+  {
+    id: 2,
     name: "Venom",
     icon: Venom,
     text: "Venom é um pequeno projeto idealizado / desenvolvido para treinar minhas skills no ReactJS,  acesse o link do build desse projeto no vercel.",
     href: "https://venom-carnage.vercel.app/",
   },
   {
-    id: 2,
+    id: 3,
     name: "Netflix",
     icon: Netflix,
     text: '"Projeto com objetivo de recriar a seleção de perfil e a home page da Netflix utilizando minhas skills atuais no ReactJS.',
     href: "https://minhanetflix.vercel.app/",
   },
   {
-    id: 3,
+    id: 4,
     name: "Nubank",
     icon: Nubank,
     text: '"Projeto com objetivo de recriar a home page do Nubank utilizando minhas skills atuais no ReactJS.',
     href: "https://nu-bank.vercel.app/",
   },
   {
-    id: 4,
+    id: 5,
     name: "Gama Learning",
     icon: Gamalearning,
     text: "Gama Academy (Learning) é um pequeno projeto E-learning, idealizado / desenvolvido para que alunos e professores possam continuar a aprender e ensinar em qualquer lugar e a qualquer hora.",
     href: "https://gamalearning.vercel.app/",
   },
   {
-    id: 5,
+    id: 6,
     name: "Casca Grossa",
     icon: CascaGrossa,
     text: "Casca Grossa é um pequeno projeto de e-commerce, idealizado / desenvolvido durante o bootcamp Gama Experience da Gama Academy.",
     href: "https://casca-grossa.vercel.app/",
   },
   {
-    id: 6,
+    id: 7,
     name: "Workpoint",
     icon: Workpoint,
     text: "Meu primeiro projeto feito com ReactJS, Case de estudo, acesse o link do build desse projeto no netlify!.",
